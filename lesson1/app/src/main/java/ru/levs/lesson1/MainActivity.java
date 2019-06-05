06267d6895e8d3b48fe611b5d9c69ee5b69e99b5
Ваня <Vanek_lev97@mail.ru>
2019-06-05 21:44:27 +0300

    Lesson 1 Задание 1 Переделал array на sortedset и сделал требуемый вывод

--- lesson1/app/src/main/java/ru/levs/lesson1/MainActivity.java
+++ lesson1/app/src/main/java/ru/levs/lesson1/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.Locale;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<auto> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonSave = (Button)findViewById(R.id.button);
         Button buttonPrint = (Button) findViewById(R.id.button2);
-        final ArrayList<String> names = new ArrayList<String>();
-        //final SortedSet<String> names = new TreeSet<>();
+        //final ArrayList<String> names = new ArrayList<String>();
+        final SortedSet<String> names = new TreeSet<>();
         final EditText text = (EditText) findViewById(R.id.editText);
         final TextView messageText = (TextView) findViewById(R.id.textView1);
         final TextView arrText = (TextView) findViewById(R.id.textView2);
@@ -53,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 arrText.setText("");
-                Collections.addAll(names);// убрать для treeset
-                Collections.sort(names, collator);// убрать для treeset
-                for(int i = 0; i < names.size(); i++) {
-                    arrText.append(names.get(i) + "\n");
+                //Collections.addAll(names);// убрать для treeset
+               // Collections.sort(names, collator);// убрать для treeset
+                //for(int i = 0; i < names.size(); i++) {
+                    //arrText.append(names.get(i) + "\n");
+                for(String i : names) {
+                    arrText.append(i + "\n");
                 }
                 //arrText.setText(names.toString() + "\n"); тут отображается так [Аня, Ваня],
                 // т.е. сортиует и удаляет повторения, но отображается неверно
