cf2536b1fb3e33b578c274b29a085c3c19418864
Ваня <Vanek_lev97@mail.ru>
2019-06-06 01:20:11 +0300

    .

--- lesson1/app/src/main/java/ru/levs/lesson1/MainActivity.java
+++ lesson1/app/src/main/java/ru/levs/lesson1/MainActivity.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.Collator;
 import java.util.ArrayList;
@@ -26,48 +24,28 @@ public class MainActivity<auto> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonSave = (Button)findViewById(R.id.button);
         Button buttonPrint = (Button) findViewById(R.id.button2);
-        //final ArrayList<String> names = new ArrayList<String>();
+//final ArrayList<String> names = new ArrayList<String>();
         final SortedSet<String> names = new TreeSet<>();
         final EditText text = (EditText) findViewById(R.id.editText);
         final TextView messageText = (TextView) findViewById(R.id.textView1);
         final TextView arrText = (TextView) findViewById(R.id.textView2);
         final Collator collator = Collator.getInstance(new Locale("ru")); //для буквы ё чоб не лезла вниз
 
-//        buttonSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                messageText.setVisibility(View.VISIBLE); // Делаю выдимым уведомление
-//                names.add(text.getText().toString());    // Заполнение массива
-//                messageText.setText("Участник добавлен");
-//                text.setText("");
-//                messageText.postDelayed(new Runnable() {  // скрываю увед через 3 секунды
-//                    @Override
-//                    public void run() {
-//                        messageText.setVisibility(View.INVISIBLE);
-//                    }
-//                }, 3000);
-//            }
-//        });
-        text.setOnKeyListener(new View.OnKeyListener() {
+        buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
-                    Toast.makeText(MainActivity.this, text.getText(), Toast.LENGTH_SHORT).show();
-
-                    messageText.setText("Участник добавлен");
-                    text.setText("");
-                    messageText.postDelayed(new Runnable() {  // скрываю увед через 3 секунды
+            public void onClick(View view) {
+                messageText.setVisibility(View.VISIBLE); // Делаю выдимым уведомление
+                names.add(text.getText().toString()); // Заполнение массива
+                messageText.setText("Участник добавлен");
+                text.setText("");
+                messageText.postDelayed(new Runnable() { // скрываю увед через 3 секунды
                     @Override
-                        public void run() {
-                            messageText.setVisibility(View.INVISIBLE);
-                        }
-                    }, 3000);
-
-
-                    return true;
-                }
-                return false;
+                    public void run() {
+                        messageText.setVisibility(View.INVISIBLE);
+                    }
+                }, 3000);
             }
         });
 
@@ -75,15 +53,15 @@ public class MainActivity<auto> extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 arrText.setText("");
-                //Collections.addAll(names);// убрать для treeset
-               // Collections.sort(names, collator);// убрать для treeset
-                //for(int i = 0; i < names.size(); i++) {
-                    //arrText.append(names.get(i) + "\n");
+//Collections.addAll(names);// убрать для treeset
+// Collections.sort(names, collator);// убрать для treeset
+//for(int i = 0; i < names.size(); i++) {
+//arrText.append(names.get(i) + "\n");
                 for(String i : names) {
                     arrText.append(i + "\n");
                 }
-                //arrText.setText(names.toString() + "\n"); тут отображается так [Аня, Ваня],
-                // т.е. сортиует и удаляет повторения, но отображается неверно
+//arrText.setText(names.toString() + "\n"); тут отображается так [Аня, Ваня],
+// т.е. сортиует и удаляет повторения, но отображается неверно
             }
         });
     }
